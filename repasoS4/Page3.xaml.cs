cd463e437f7f6ac6af31b256e5a5707662df31f4
master <master@SMLAB306PC05_23>
2023-06-19 21:31:21 -0500

    Agregar archivos de proyecto.

---
+++ repasoS4/Page3.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace repasoS4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page3 : ContentPage
+    {
+        public Page3(String usuario, double dato, string alumno)
+        {
+            InitializeComponent();
+            lblUusario.Text = "USUARIO CONECTADO: "+ usuario;
+            txtDato.Text = dato.ToString();
+            txtAlumno.Text = alumno;
+        }
+    }
+}
